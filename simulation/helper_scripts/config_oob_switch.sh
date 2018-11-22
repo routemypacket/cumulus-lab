dd9f5df6caa989895ebad57641ade65a7b30950d
Ahmed Jama <ahmed.jama@thehutgroup.com>
2018-11-22 10:04:59 +0000

    Modified topology and Vagrant file

--- simulation/helper_scripts/config_oob_switch.sh
+++ simulation/helper_scripts/config_oob_switch.sh
@@ -17,13 +17,12 @@ iface eth0 inet dhcp
 auto bridge
 iface bridge
     alias Untagged Bridge
-    bridge-ports swp1 swp2 swp3 swp4 swp5 swp6 swp7 swp8 swp9 swp10 swp11 swp12 swp13 swp14 swp15
+    bridge-ports swp1 swp2 swp3 swp4 swp5 swp6 swp7 swp8 swp9 swp10 swp11 swp12 swp13 swp14 swp15 swp16
     hwaddress a0:00:00:00:00:61
     address 192.168.0.1/24
-    
+
 EOT
 
 echo "#################################"
 echo "   Finished "
 echo "#################################"
-
